48ffb689b0845ce7f9e205ed0544c03eaa4664cf
Ruslan <ruslan24krut@gmail.com>
2024-07-23 17:26:08 +0300

    Добавил try-catch блок

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -9,28 +9,32 @@ import java.time.Duration;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args){
         WebDriver driver = new ChromeDriver();
         String expectedText = "1 товар";
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
 
-        driver.get("https://www.wildberries.ru/");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='main-page__content']/descendant::article[1]/div/a")));
-        driver.findElement(By.id("searchInput")).sendKeys("мобильный телефон");
+        try {
+            driver.get("https://www.wildberries.ru/");
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='main-page__content']/descendant::article[1]/div/a")));
+            driver.findElement(By.id("searchInput")).sendKeys("мобильный телефон");
 
-        Thread.sleep(1000);
-        driver.findElement(By.id("applySearchBtn")).click();
+            Thread.sleep(1000);
+            driver.findElement(By.id("applySearchBtn")).click();
 
-        WebElement card = wait.until(ExpectedConditions.visibilityOfElementLocated(By
-                .xpath("//div[@class='product-card-list']/descendant::article[1]/div/a")));
-        card.click();
+            WebElement card = wait.until(ExpectedConditions.visibilityOfElementLocated(By
+                    .xpath("//div[@class='product-card-list']/descendant::article[1]/div/a")));
+            card.click();
 
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label = 'Добавить в корзину']")))
-                .click();
-        driver.findElement(By.xpath("//span[@class = 'navbar-pc__icon navbar-pc__icon--basket']")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label = 'Добавить в корзину']")))
+                    .click();
+            driver.findElement(By.xpath("//span[@class = 'navbar-pc__icon navbar-pc__icon--basket']")).click();
 
-        WebElement product = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'accordion__goods-count']")));
-        assertEquals(expectedText, product.getText());
+            WebElement product = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class = 'accordion__goods-count']")));
+            assertEquals(expectedText, product.getText());
+        } catch (InterruptedException e) {
+            e.fillInStackTrace();
+        }
     }
 
 }
